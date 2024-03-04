85927cd76dbf0be458f416fb62d4fd1444100f6c
SaiyedRushan <rushan52@gmail.com>
2024-03-03 22:37:29 -0500

    publish message on customer creation to notification queue

--- customer/src/main/java/org/rushan/CustomerService.java
+++ customer/src/main/java/org/rushan/CustomerService.java
@@ -1,10 +1,13 @@
 package org.rushan;
 
+
+import org.rushan.notification.NotificationRequest;
+import org.rushan.fraud.FraudCheckResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
+public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate, RabbitMQMessageProducer rabbitMQMessageProducer) {
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -24,5 +27,18 @@ public record CustomerService(CustomerRepository customerRepository, RestTemplat
         if(fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
         }
+
+        NotificationRequest notificationRequest = new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s, Welcome to the matrix.",
+                        customer.getFirstName())
+        );
+
+        rabbitMQMessageProducer.publish(
+                notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key"
+        );
     }
 }
